476d71c5bb2cdc4ca2bc4fa5a5c2df8ea505b294
Jianfei Zheng <jiz2@hi.is>
2015-11-29 21:25:51 +0000

    Removed Searcher

--- src/main/java/project/controller/SearchController.java
+++ src/main/java/project/controller/SearchController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import project.persistence.entities.SoundClip;
 import project.service.DatabaseConnector;
-import project.service.Searcher;
 
 /**
  *
