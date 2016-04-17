da6d7ed68b4cccb248612a39c3e15d0689e8aa9f
Jianfei Zheng <jiz2@hi.is>
2016-04-17 16:32:42 +0000

    Updated createApp.sql for DB

--- createApp.sql
+++ createApp.sql
@@ -9,6 +9,15 @@ CREATE TABLE soundclip
   CONSTRAINT soundclips_pkey PRIMARY KEY (id)
 );
 
+CREATE TABLE users
+(
+  id bigint NOT NULL,
+  name character varying(255) NOT NULL,
+  hash character varying(255) NOT NULL,
+  CONSTRAINT users_pkey PRIMARY KEY (id),
+  CONSTRAINT users_name_unique UNIQUE (name)
+);
+
 CREATE ROLE soundup LOGIN
   ENCRYPTED PASSWORD 'md54e59dba9d8cb443b9aa5fe6ac162ce8e'
   SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
