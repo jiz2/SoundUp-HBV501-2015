15f141e97b08f31173e590805992c2b5b39de3d7
DanniD3 <daa11@hi.is>
2016-04-18 12:42:32 +0000

    db update

--- createApp.sql
+++ createApp.sql
@@ -1,19 +1,19 @@
 CREATE TABLE soundclip
 (
-  id bigint NOT NULL,
+  id bigserial NOT NULL,
   name character varying(255),
   ext character varying(10) NOT NULL,
   uploader character varying(255),
   data character varying NOT NULL,
-  private boolean NOT NULL,
+  isPrivate boolean NOT NULL,
   CONSTRAINT soundclips_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE users
 (
-  id bigint NOT NULL,
+  id bigserial NOT NULL,
   name character varying(255) NOT NULL,
-  hash character varying(255) NOT NULL,
+  pw character varying(255) NOT NULL,
   CONSTRAINT users_pkey PRIMARY KEY (id),
   CONSTRAINT users_name_unique UNIQUE (name)
 );
